e0314f3cc8b7e6704fb527a993c8f7a966b286da
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-02-14 21:33:10 +0200

    post data to the server after getting it from the api

--- website/app.js
+++ website/app.js
@@ -3,30 +3,40 @@ const apiKey = '97223487ae84a942296e1f01d1b6175e';
 
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let userResponse;
+let temp;
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e){
     let newZip =  document.getElementById('zip').value;
+    userResponse =  document.getElementById('feelings').value;
        console.log(newZip);
     getZipCode(baseUrl,newZip, apiKey).then(
-        app.post('/newData', addData)
+        postData('/newData', {
+            temperature: temp,
+             date: newDate,
+              userResponse: userResponse,
+            })
     );
+   
 }
 
 const getZipCode = async (baseURL, zip, key)=>{
    
 console.log('trying to fatch');
 
-  const response = await fetch(baseURL+zip+'&appid='+key);
-  let temp = response.main;
+  const response = await fetch(baseURL+zip+'&appid='+key+"&units=metric");
+ 
   console.log(temp);
   try {
     const data = await response.json();
-    let temp = data.main.temp;
-   
-
-    console.log("it should work here");
-
+   temp = data.main.temp;
+    
+    console.log(temp);
+console.log(userResponse);
+console.log(newDate);
    
     return data;
   }  catch(error) {
@@ -38,20 +48,27 @@ console.log('trying to fatch');
 
 
 // post the data wh got from the api
-function addData(req,res){
-    const d = new Date();
-    
-    newEntry = {
-      temp: res.main.temp,
-      date:d.getTime(),
-      userResponse:  document.getElementById('feelings').value,
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+             return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
     }
-  
-    weatherData.push(newEntry)
-    console.log(weatherData);
-  }
+}
+
+
 // Create a new date instance dynamically with JS
 
 
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
